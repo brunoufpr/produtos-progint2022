867984bde3f94beb32121318fb517c3457dd67fc
Bruno Cruz <brunocruz127@gmail.com>
2022-09-12 16:06:45 -0300

    commit faltante

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import json
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
 import psycopg2
 import psycopg2.extras
-from model import Produto, db
-from flask_migrate import Migrate
+from model import Filial, Produto, db
+from flask_migrate import Migrate 
+
+from flask_session import Session
+
+
+
+
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bruno123@localhost:5432/produtos'
 db.init_app(app)
 migrate= Migrate(app,db)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+ 
 
 @app.route("/")
 def home():
@@ -18,7 +29,8 @@ def home():
 @app.route("/produtos")
 def produtos ():
     #Carregar filial
-    id_f = 1  ## TODO: apos fazer o login, substituir pelo usuario logado
+    id_f = session["filial"].id
+    print (session["filial"].__dict__)
 
     #Carregar os produtos do Banco
     produtos = Produto.query.filter_by(id_filial=id_f).all()  # lista de produtos. Ex: [p1,p2,p3]
@@ -46,3 +58,27 @@ def cadastrar_produto_action():
     db.session.commit()
 
     return redirect(url_for("produtos"))
+
+
+
+
+@app.get("/login_form")
+def login_form():
+    return render_template( "login.html")
+
+@app.post("/login_action")
+def login_action():
+    username=request.form.get("username")
+    senha=request.form.get("senha")
+    print(username,senha)
+    filial= Filial.query.filter_by(username=username).first()
+
+    if not filial:
+        return "filial nao existe"
+    if filial.check_password(senha):
+        return "senha incorreta"
+
+    session["filial"]=filial
+
+
+    return render_template ("produtos.html")
